fb18b2d7b2ac12cc711919a847bc45a5863621a7
GeneYao <geneyao0302@gmail.com>
2019-07-18 15:56:02 +0800

    input complete

--- Gene_pa2/source.cpp
+++ Gene_pa2/source.cpp
@@ -4,8 +4,12 @@
 #include <boost/polygon/polygon.hpp>
 
 using namespace std;
-namespace gtl = boost::polygon;
+using namespace boost::polygon;
 using namespace boost::polygon::operators;
+
+typedef polygon_data<int> Polygon;
+typedef polygon_traits<Polygon>::point_type Point;
+typedef vector<Polygon> PolygonSet;
 
 class Merge
 {
@@ -32,6 +36,7 @@ int main(int argc, char *argv[])
 
     /* ----- Read Input File ----- */
     string input;    
+    int inputNum;
     vector<string> operation;
 
     while (fin >> input) {
@@ -42,23 +47,39 @@ int main(int argc, char *argv[])
                 operation.push_back(input);
             }
         }
-        else if (input.compare("END") == 0) {
-            fin >> input;
-            continue;
-        }
+        // else if (input.compare("END") == 0) {
+        //     fin >> input;
+        //     continue;
+        // }
         /* ----- Store Opertion Information ----- */        
         else if (input.compare("DATA") == 0) {
-            fin >> input; 
-            /* ----- Store Merge Polygon ----- */
-            if (input.compare("MERGE")) {
-                fin >> input;
-                cout << input << endl;
-            } 
-            /* ----- Store Clipper Polygon ----- */
-            else if (input.compare("CLIPPER")) {
-                fin >> input;
-                cout << input << endl;
-            }    
+            /* ----- input phrase "Merge" or "Clipper"*/
+            fin >> input;
+            /* ----- input operation name ----- */
+            fin >> input;
+            cout << input << endl; 
+            // /* ----- Store Merge Polygon ----- */
+            // if (input.compare("MERGE")) {
+            //     fin >> input;
+            //     cout << input << endl; 
+                                          
+            // } 
+            // /* ----- Store Clipper Polygon ----- */
+            // else if (input.compare("CLIPPER")) {
+            //     fin >> input;
+            //     cout << input << endl;
+            // }
+            /* ----- Read Polygon ----- */
+            for (; input.compare("END") != 0;fin >> input) {            
+                if (input.compare("POLYGON") == 0) {
+                    fin >> input;
+                        for (; input.compare(";") != 0;fin >> input) {                
+                            cout << input << endl;;
+                        }
+                    }
+            }
+            /* ----- End ----- */
+            fin >> input;     
         }
     }
 
